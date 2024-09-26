4ea3c1ac0d3ab976fe922ec3557bdd6630a1b113
LoupMr <loup.marquez@epitech.eu>
2024-09-26 02:54:36 +0200

    feat(Social Login): Added Option to Log with google, github and discord

--- nell_backend/authentication/urls.py
+++ nell_backend/authentication/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from .views import RegisterView, google_login, github_login, discord_login
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    # JWT Login (obtain token)
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    
-    # JWT Token Refresh
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
+
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+
+    # Social login URLs
+    path('dj-rest-auth/google/login/', google_login, name='google_login'),
+    path('dj-rest-auth/github/login/', github_login, name='github_login'),
+    path('dj-rest-auth/discord/login/', discord_login, name='discord_login'),
+
+    # Allauth Social login URLs
+    path('accounts/', include('allauth.urls')),
 ]
--- nell_backend/authentication/views.py
+++ nell_backend/authentication/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
+from allauth.socialaccount.providers.google.views import oauth2_login as google_login_view
+from allauth.socialaccount.providers.github.views import oauth2_login as github_login_view
+from allauth.socialaccount.providers.discord.views import oauth2_login as discord_login_view
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+    @swagger_auto_schema(request_body=RegisterSerializer)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({"message": "User registered successfully."}, status=status.HTTP_201_CREATED)
+
+# Social login views wrapped for Swagger
+@swagger_auto_schema(method='get', operation_description="Login with Google OAuth2")
+@api_view(['GET'])
+def google_login(request):
+    return google_login_view(request)
+
+@swagger_auto_schema(method='get', operation_description="Login with GitHub OAuth2")
+@api_view(['GET'])
+def github_login(request):
+    return github_login_view(request)
+
+@swagger_auto_schema(method='get', operation_description="Login with Discord OAuth2")
+@api_view(['GET'])
+def discord_login(request):
+    return discord_login_view(request)
