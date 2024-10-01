658bd50a5e04f62bab90cea139ec58a7be97f9ab
noe <90001579+nkurata@users.noreply.github.com>
2024-10-01 18:44:46 +0200

    feat: start of mobile login page

--- area/lib/login_form.dart
+++ area/lib/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 import 'shared_widgets.dart'; // Import shared widgets like SocialLoginButtons
 import 'forms_field.dart'; // Import form fields like EmailField and PasswordField
 
@@ -14,8 +15,8 @@ class LoginPage extends StatelessWidget {
       ),
       body: Center(
         child: SizedBox(
-          width: 800,
-          height: 600,
+          width: Platform.isAndroid || Platform.isIOS ? double.infinity : 800,
+          height: Platform.isAndroid || Platform.isIOS ? double.infinity : 600,
           child: Stack(
             children: [
               Positioned.fill(
@@ -25,51 +26,79 @@ class LoginPage extends StatelessWidget {
                     color: const Color.fromARGB(255, 140, 211, 255),
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.all(20.0),
-                          child: const Text(
-                            "Connect your favourite apps and automate workflows.",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 45,
-                              fontWeight: FontWeight.bold,
+                  child: Platform.isAndroid
+                      ? Center(
+                          child: const AuthContainer(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Login Mobile",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                EmailField(),
+                                SizedBox(height: 20),
+                                PasswordField(),
+                                SizedBox(height: 20),
+                                SocialLoginButtons(),
+                                SizedBox(height: 20),
+                                LoginButton(),
+                                SizedBox(height: 20),
+                                RegisterLink(),
+                              ],
                             ),
-                            textAlign: TextAlign.left,
                           ),
-                        ),
-                      ),
-                      const Expanded(
-                        child: AuthContainer(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Login",
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
+                        )
+                      : Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.all(20.0),
+                                child: const Text(
+                                  "Connect your favourite apps and automate workflows.",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 45,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.left,
                                 ),
                               ),
-                              SizedBox(height: 20),
-                              EmailField(),
-                              SizedBox(height: 20),
-                              PasswordField(),
-                              SizedBox(height: 20),
-                              SocialLoginButtons(),
-                              SizedBox(height: 20),
-                              LoginButton(),
-                              SizedBox(height: 20),
-                              RegisterLink(),
-                            ],
-                          ),
+                            ),
+                            const Expanded(
+                              child: AuthContainer(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 20),
+                                    EmailField(),
+                                    SizedBox(height: 20),
+                                    PasswordField(),
+                                    SizedBox(height: 20),
+                                    SocialLoginButtons(),
+                                    SizedBox(height: 20),
+                                    LoginButton(),
+                                    SizedBox(height: 20),
+                                    RegisterLink(),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
                 ),
               ),
             ],
