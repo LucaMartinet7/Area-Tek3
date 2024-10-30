c9447148ccef4edbef9608f48fa0af5519e4f017
Loup Marquez-Luttringer <loup.marquez@epitech.eu>
2024-10-30 17:11:52 +0100

    feat: working solution

--- nell_backend/spotifys/urls.py
+++ nell_backend/spotifys/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('callback/', spotify_callback, name='spotify_callback'),
     path('playlists/', get_user_playlists, name='get_user_playlists'),
     path('check-beatles/', check_spotify_beatles, name='check_spotify_beatles'),
-    path('check-spotify-song/', SpotifySongActionViewSet.as_view(), name='check_spotify_song'),
+    #path('check-spotify-song/', SpotifySongActionViewSet.as_view(), name='check_spotify_song'),
     path('send-twitch-message/', TwitchChatReactionViewSet.as_view(), name='send_twitch_message'),
     path('', include(router.urls)),
 ]
