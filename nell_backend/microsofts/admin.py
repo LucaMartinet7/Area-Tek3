cc4b725ea3ad3095e08efc166e19a2a76b79e6b0
Tounn10 <etienne.namur@epitech.eu>
2024-11-01 18:27:16 +0100

    del: model deleted was still registered

--- nell_backend/microsofts/admin.py
+++ nell_backend/microsofts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(OutlookReaction)
 admin.site.register(CalendarEventReaction)
 admin.site.register(OneDriveFile)
 admin.site.register(GoogleDriveFileReaction)
