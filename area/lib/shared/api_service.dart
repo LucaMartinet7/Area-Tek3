72fc261b50ed2000c3da7b55385e70d8fb2c73a0
noé <noe.kurata@epitech.eu>
2024-11-01 13:47:03 +0100

    fix: remove useless import

--- area/lib/shared/api_service.dart
+++ area/lib/shared/api_service.dart
@@ -1,6 +1,5 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
