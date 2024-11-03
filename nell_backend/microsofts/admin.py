12f10799cbb366306a3b01f54c7027757b13c519
Tounn10 <etienne.namur@epitech.eu>
2024-11-03 15:46:10 +0100

    del: microsoft service

--- nell_backend/microsofts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(CalendarEventReaction)
-admin.site.register(OneDriveFile)
-admin.site.register(GoogleDriveFileReaction)
-admin.site.register(OutlookEmailAction)
-admin.site.register(GoogleChatMessageReaction)
--- nell_backend/microsofts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MicrosoftConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'microsofts'
