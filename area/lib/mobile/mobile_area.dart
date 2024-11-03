abae0dafe18034e08d3a8abcced7e48085738a5c
noé <noe.kurata@epitech.eu>
2024-11-03 13:27:55 +0100

    feat: fix release about buttons

--- area/lib/mobile/mobile_area.dart
+++ area/lib/mobile/mobile_area.dart
@@ -61,7 +61,7 @@ class MobileActionReactionPage extends StatelessWidget {
                     ),
                   ),
                 );
-              }).toList(),
+              }),
             ],
           ),
         ),
